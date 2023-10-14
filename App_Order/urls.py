52076cf0e6bb86cbb65d6e03f8c9f8a7292edd85
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-10-14 13:02:44 +0600

    added add_to_card feature from App_Order

--- App_Order/urls.py
+++ App_Order/urls.py
@@ -4,6 +4,8 @@ from App_Order import views
 app_name='App_Order'
 
 urlpatterns = [
-   
+    path('add/<pk>/', views.add_to_cart, name="add"),
+      path('cart/', views.cart_view, name="cart"),
 
 ]
+
