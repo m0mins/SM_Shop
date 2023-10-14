5d95ba46626fb632dfd10fc1b153f5440e3d916d
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-10-14 12:23:38 +0600

    Card and Order model done for App_Order

--- SM_Shop_Proj/settings.py
+++ SM_Shop_Proj/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "crispy_bootstrap4",
     'App_Login',
     'App_Shop',
+    'App_Order',
     'django_cleanup.apps.CleanupConfig',
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
--- SM_Shop_Proj/urls.py
+++ SM_Shop_Proj/urls.py
@@ -5,8 +5,10 @@ from django.contrib.staticfiles.urls import static ,staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('',include('App_Shop.urls')),
     path('account/',include('App_Login.urls')),
-    path('shop/',include('App_Shop.urls')),
+    path('shop/',include('App_Order.urls')),
+
 ]
 urlpatterns +=staticfiles_urlpatterns()
 urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
