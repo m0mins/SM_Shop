a90c5ee8323bc247959870949b6ea10e4aa5305b
m0mins <momin.pstu.cse11.bd@gmail.com>
2023-11-15 19:37:45 +0600

    changed

--- SM_Shop_Proj/settings.py
+++ SM_Shop_Proj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6x707j2nls#satoy92v$wbb@hibp(awyj42ew@4awjvm(&y&y)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
